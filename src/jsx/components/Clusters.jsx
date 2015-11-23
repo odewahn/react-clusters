9e3f3dae23820bca5d0abe95e5e76aafcdc2118f
Andrew Odewahn <andrew@odewahn.com>
2015-11-23 07:42:12 -0500

    truncating long string names for cert path

--- src/jsx/components/Clusters.jsx
+++ src/jsx/components/Clusters.jsx
@@ -37,20 +37,30 @@ module.exports = React.createClass({
   },
   render: function(){
     var clusterList = this.props.clusters.ClusterList.map(function(c){
+
+      //shortens a given string and adds elipses
+      function shorten(s, n) {
+        var out = s;
+        if ((s.length - 3) > n) {
+          out = s.slice(0,n-3)+"..."
+        }
+        return out;
+      }
+
       return (
         <tr>
           <td><input type="radio" name="default" value="{c.Name}"></input></td>
           <td>{c.DockerHost}</td>
           <td>{c.Name}</td>
           <td>{c.PopulatedBy}</td>
-          <td>{c.TLSCaCert}</td>
-          <td>{c.TLSCert}</td>
-          <td>{c.TLSKey}</td>
+          <td>{shorten(c.TLSCaCert,25)}</td>
+          <td>{shorten(c.TLSCert,25)}</td>
+          <td>{shorten(c.TLSKey,25)}</td>
         </tr>
       )
     });
     return (
-      <div id='fred' style={{border: '1px solid red'}} >
+      <div>
         <h3>These are your clusters</h3>
         <table>
           <thead>
@@ -65,7 +75,7 @@ module.exports = React.createClass({
           </tr>
         </thead>
         <tbody>
-          {clusterList}          
+          {clusterList}
         </tbody>
         </table>
       </div>
